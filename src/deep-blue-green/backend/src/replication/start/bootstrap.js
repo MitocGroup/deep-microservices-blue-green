11ece1170c1fe8db029491e656b61cea2b2b4bf7
CCristi <covali.christian@gmail.com>
2017-02-16 19:13:36 +0200

    # Codereview fixes

--- src/deep-blue-green/backend/src/replication/start/bootstrap.js
+++ src/deep-blue-green/backend/src/replication/start/bootstrap.js
@@ -8,15 +8,17 @@
 
 const AWS = require('aws-sdk');
 const CHUNK_SIZE = 100;
+const TableManager = require('./TableManager');
+const Marshaler = require('./Marshaler');
+const Item = require('./Item');
+const BackFillExecutor = require('./BackFillExecutor');
 
 exports.handler = (event, context, callback) => {
   const tableManager = new TableManager(new AWS.DynamoDB(), event.Table);
   const backFillExecutor = new BackFillExecutor(tableManager, event.Hash);
   const scanPayload = {};
 
-  scanPayload[':hash'] = {
-    S: event.Hash,
-  };
+  scanPayload[':hash'] = Marshaler.marshalItem(event.Hash);
 
   const backFillProcessor = () => {
     tableManager
@@ -46,159 +48,6 @@ exports.handler = (event, context, callback) => {
 
   backFillProcessor();
 };
-
-class BackFillExecutor {
-  constructor(tableManager, hash) {
-    this._tableManager = tableManager;
-    this._hash = hash;
-  }
-
-  backFillChunks(chunks) {
-    if (chunks.length === 0) {
-      return Promise.resolve();
-    }
-
-    let chunksClone = [].concat(chunks);
-    let currentChunk = chunksClone.shift();
-
-    console.log(`Updating "${currentChunk.length}" items.`);
-
-    return this._backFillSingleChunk(currentChunk)
-      .then(() => this.backFillChunks(chunksClone))
-  }
-
-  _backFillSingleChunk(chunk) {
-    return Promise.all(
-      chunk.map(this._backFillSingleItem.bind(this))
-    );
-  }
-
-  _backFillSingleItem(item) {
-    let updateValues = {};
-
-    updateValues[':hash'] = {
-      S: this._hash,
-    };
-
-    return this._tableManager.updateItem(
-      item,
-      BackFillExecutor.BACKFILL_UPDATE_EXPRESSION,
-      updateValues
-    );
-  }
-
-  static get BACKFILL_UPDATE_EXPRESSION() {
-    return `SET ${TableManager.BACKFILL_FIELD} = :hash`;
-  }
-}
-
-class TableManager {
-  constructor(dynamoDb, tableName) {
-    this._dynamoDb = dynamoDb;
-    this._tableName = tableName;
-  }
-
-  updateItem(item, updateExpression, updateValues) {
-    return this._dynamoDb.updateItem(
-      this._buildUpdatePayload(item, updateExpression, updateValues)
-    ).promise();
-  }
-
-  scan(fields, filterExpression, filterValues) {
-    return this._dynamoDb.scan(
-      this._buildScanPayload(fields, filterExpression, filterValues)
-    ).promise();
-  }
-
-  _buildUpdatePayload(item, updateExpression, updateValues) {
-    let payload = {};
-
-    payload.TableName = this._tableName;
-    payload.Key = {Id: {S: item.Id}};
-    payload.UpdateExpression = updateExpression;
-    payload.ExpressionAttributeValues = updateValues;
-    payload.ReturnValues = 'NONE';
-    payload.ReturnConsumedCapacity = 'NONE';
-    payload.ReturnItemCollectionMetrics = 'NONE';
-
-    return payload;
-  }
-
-  _buildScanPayload(fields, filterExpression, filterValues) {
-    let payload = {};
-
-    payload.TableName = this._tableName;
-    payload.FilterExpression = filterExpression;
-    payload.ProjectionExpression = fields.map(f => `#${f}`).join(',');
-    payload.ExpressionAttributeNames = fields.reduce((obj, f) => ((obj[`#${f}`] = f) && obj), {});
-    payload.ExpressionAttributeValues = filterValues;
-    payload.ReturnConsumedCapacity = 'NONE';
-
-    return payload;
-
-  }
-
-  /**
-   * @returns {String}
-   */
-  static get BACKFILL_FILTER() {
-    return `attribute_not_exists(${TableManager.BACKFILL_FIELD}) or ${TableManager.BACKFILL_FIELD} <> :hash`;
-  }
-
-  /**
-   * @returns {String}
-   */
-  static get BACKFILL_FIELD() {
-    return 'DeepReplicationHash';
-  }
-}
-
-class Item {
-  constructor(rawData) {
-    this._rawData = rawData;
-    this._data = this.extract();
-  }
-
-  extract() {
-    let data = {};
-
-    for (let key in this._rawData) {
-      if (!this._rawData.hasOwnProperty(key)) {
-        continue;
-      }
-
-      for (let type in this._rawData[key]) {
-        if (this._rawData[key].hasOwnProperty(type)) {
-          data[key] = (Serializer[type] || Serializer.defaultSerializer)(this._rawData[key][type]);
-        }
-      }
-    }
-
-    return data;
-  }
-
-  get fields() {
-    return Object.keys(this._rawData);
-  }
-
-  get Id() {
-    return this._data.Id;
-  }
-
-  get data() {
-    return this._data;
-  }
-}
-
-class Serializer {
-  static S(i) {
-    return i.toString();
-  }
-
-  static defaultSerializer(i) {
-    return Serializer.S(i);
-  }
-}
 
 function arrayChunkReducer(chunkSize) {
   return function(chunks, item) {
